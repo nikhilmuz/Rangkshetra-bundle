e12b3a2b37f1edf309ccccd62bc2767b3d59e040
Nikhil Kumar <nikhil.nikhil.muz@gmail.com>
2018-11-05 22:00:28 +0530

    201811052200

---
+++ precache-manifest.dfa8523e9a5a747ae8669cc9b7f8c752.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7d5297ac13b8f6dc7706dc99f5faa0c",
+    "url": "/static/media/tbsbanner1.f7d5297a.jpg"
+  },
+  {
+    "revision": "be57b4bbb484ec79cd6d",
+    "url": "/static/css/main.2b9a74f4.chunk.css"
+  },
+  {
+    "revision": "27a7bf064ddd66ae9b03b65d8e8585d5",
+    "url": "/static/media/tbsbanner6.27a7bf06.jpg"
+  },
+  {
+    "revision": "aa8f32e63fe9ec444a68",
+    "url": "/static/js/1.aa8f32e6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a9ab7ef16e711534735910c8be406b04",
+    "url": "/static/media/tbs_logo.a9ab7ef1.png"
+  },
+  {
+    "revision": "be57b4bbb484ec79cd6d",
+    "url": "/static/js/main.be57b4bb.chunk.js"
+  },
+  {
+    "revision": "9261b91b000e87c9ca2c5d54865ae8a7",
+    "url": "/static/media/tbsbanner2.9261b91b.jpg"
+  },
+  {
+    "revision": "e710fab0535882c7e1994793e5671bbb",
+    "url": "/static/media/tbsbanner3.e710fab0.jpg"
+  },
+  {
+    "revision": "fa9a2f18b032be2330aef90c261b1afb",
+    "url": "/static/media/tbsbanner4.fa9a2f18.jpg"
+  },
+  {
+    "revision": "1ce9eddf46c8e9ddf955f0de5bcc8500",
+    "url": "/static/media/tbsbanner5.1ce9eddf.jpg"
+  },
+  {
+    "revision": "aa8f32e63fe9ec444a68",
+    "url": "/static/css/1.7a2b4cda.chunk.css"
+  },
+  {
+    "revision": "d3f68e6e1f15a7f9e7ad0a2495cdfdee",
+    "url": "/index.html"
+  }
+];
